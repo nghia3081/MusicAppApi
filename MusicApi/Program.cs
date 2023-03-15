78fdbb96491b2f5ea8c53cce91d19fec0da8de5b
Nghia Nguyen <nhiep2371@gmail.com>
2023-03-15 11:09:23 +0700

    fix bug

--- MusicApi/Program.cs
+++ MusicApi/Program.cs
@@ -13,11 +13,10 @@ builder.WebHost.UseKestrel().UseIISIntegration();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
